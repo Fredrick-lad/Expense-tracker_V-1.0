14c09e92cdfccaaf08d3e81d279674804c86ea71
Fredrck-lad <fredrick8283@gmail.com>
2025-11-03 10:59:55 +0300

    removed unknown package

--- src/main/java/com/daily_expense_tracker/Expense_Tracker_V_10/Application.java
+++ src/main/java/com/daily_expense_tracker/Expense_Tracker_V_10/Application.java
@@ -3,9 +3,20 @@ package com.daily_expense_tracker.Expense_Tracker_V_10;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.daily_expense_tracker.model") // where the entities are
+@EntityScan(basePackages = {
+    "com.daily_expense_tracker.model"  // Where your entities are
+})
+@EnableJpaRepositories(basePackages = {
+    "com.daily_expense_tracker.repository"
+})
+@ComponentScan(basePackages = {
+    "com.daily_expense_tracker" ,
+	"com.daily_expense_traacker.service" // Where your services and other components are
+})
 public class Application {
 
 	public static void main(String[] args) {
